e35928749951f6a02fb3c5a1eb9fcad0c109a4fd
sanakstore <144905341+sanakstore@users.noreply.github.com>
2024-06-05 11:43:08 +0800

    sanak store

--- v3.sh
+++ v3.sh
@@ -533,7 +533,7 @@ print_success "Password SSH"
 function udp_mini(){
 clear
 print_install "Install Service Limit IP"
-wget https://raw.githubusercontent.com/sanakstore/sanakstore2/main/limit/limit.sh && chmod +x limit.sh && ./limit.sh
+wget http://sc.kuhing.me/limit/limit.sh && chmod +x limit.sh && ./limit.sh
 
 cd
 wget -q -O /usr/bin/limit-ip "${REPO}limit/limit-ip"
@@ -841,7 +841,7 @@ print_success "WebSocket Proxy"
 function ins_udp(){
 clear
     print_install "INSTALL UDP CUSTOM"
-    wget -q https://raw.githubusercontent.com/sanakstore/sanakstore2/main/udp-custom.sh &&  chmod +x udp-custom.sh && ./udp-custom.sh
+    wget -q http://sc.kuhing.me/udp-custom.sh &&  chmod +x udp-custom.sh && ./udp-custom.sh
     print_success "UDP CUSTOM"
 }
 
@@ -882,7 +882,7 @@ print_success "Restart All Service"
 function menu(){
     clear
     print_install "Install Menu Packet"
-    wget --no-check-certificate https://raw.githubusercontent.com/sanakstore/sanakstore2/main/menu.zip
+    wget --no-check-certificate http://sc.kuhing.me/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
